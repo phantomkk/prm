2483c43db03a1a0a04af2fedf3eb97a0521eef30
LucNS <luckyfire1996@gmail.com>
2017-09-12 17:44:48 +0700

    [LucNS] voc code

--- app/src/main/java/com/example/lucky/barcodechecker/MainActivity.java
+++ app/src/main/java/com/example/lucky/barcodechecker/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.lucky.barcodechecker;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActivityChooserView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //hello luc
+    }
+    public void heleo(){
+        //asdfasdfjoasdfnsodfn
     }
 }
