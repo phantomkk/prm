60dfb6707ed97e402fa46cf764be1db240db60bd
DESKTOP-I6J6C3U\Lenovo <mr.trinhvo1996@gmail.com>
2017-10-10 17:04:27 +0700

    hello

--- android_source/app/src/main/java/com/project/barcodechecker/activities/MainActivity.java
+++ android_source/app/src/main/java/com/project/barcodechecker/activities/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setView();
+        ////
         actionbar = getSupportActionBar();
         actionbar.setTitle("Scan");
         pService = APIUtils.getPService();
